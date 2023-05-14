4f124ae0832418a66b0cd63363974a65dbbb2be5
Israel Sanchez <ishisra14@hotmail.com>
2023-05-13 21:09:47 -0600

    Twitter Card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { TwiterFollowCard } from './TwiterFollowCard'
+
+const users = [
+  {
+    userName: 'isra-14',
+    name: 'Israel Sanchez',
+    isFollowing: true
+  },
+  {
+    userName: 'diegommtz',
+    name: 'Diego Montoya',
+    isFollowing: false
+  },
+  {
+    userName: 'RodrigoSebastian',
+    name: 'Rodrigo Sebastian',
+    isFollowing: true
+  }
+]
+
+export function App() {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+            <TwiterFollowCard
+              userName={userName}
+              initialIsFollowing={isFollowing}
+              key={userName}
+            >
+              {name}
+            </TwiterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
